b6cc8b60ef11895b423ed8f0e36a281e982380c6
Zulfikar Skywalker <31248833+lzulfikar22@users.noreply.github.com>
2019-04-20 08:30:01 +0700

    Delete init.php

--- cookie/init.php
+++
@@ -1,9 +0,0 @@
-<?php
-$cookie_name = "user";
-$cookie_value = $_POST["nama"];
-setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
-?>
-<?php
-header("Location: index.php");
-exit;
-?>
